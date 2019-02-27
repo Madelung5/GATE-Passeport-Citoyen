dca6eb7579de09d5c3190537fc72c590cab3f464
Constance Chou <rico@minet.net>
2019-02-27 14:00:02 +0100

    Ajout de Passport.js et de la requête de login à la BDD

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json() )
@@ -13,6 +15,7 @@ password : 'Passeport',
 database : 'passeportCitoyen'
 });
 
+
 // création du serveur
 const app = express();
 
@@ -23,10 +26,38 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log('Inside POST / callback function')
   console.log(req.body)
+  const name = req.body.username;
+  const password = req.body.psw;
   res.send(`POST du login\n`)
 })
+
+// configuration de Passport.js en local strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'pseudo' },
+  (name, password, done) => {
+    console.log('Inside local strategy callback')
+    var sql = 'SELECT * FROM loginEleve WHERE user = ' + connection.escape(name);
+    connection.query(sql, function(err, results) {
+    if (err) {
+        res.send("Error during MySql command : " + err);
+    } 
+    else {
+        const passwordDB = results[0].password;
+        if (password == passwordDB) {
+            console.log('Local strategy returned true')
+            return done(null, user)
+        }
+        else {
+        res.send("Bad Password");
+        }
+    }
+  });
+  }
+));
 
 // Ecoute sur le port 8080
 app.listen(8080, () => {
     console.log('Listening on localhost:8080');
 })
+
+
