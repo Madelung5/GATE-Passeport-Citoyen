47f918d6500b88009ff56cc9b1f6672522d4c03b
Constance Chou <rico@minet.net>
2019-02-27 13:55:30 +0100

    Ajout d'une méthode POST pour le login sur le /

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname +'/accueil.html')
 });
 
+app.post('/', (req, res) => {
+  console.log('Inside POST / callback function')
+  console.log(req.body)
+  res.send(`POST du login\n`)
+})
+
 // Ecoute sur le port 
 app.listen(8080, () => {
     console.log('Listening on localhost:8080');
