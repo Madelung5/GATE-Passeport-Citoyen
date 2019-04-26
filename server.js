1ae866b7e00ccafe8dbd7714c636a49c47db83b6
Constance Chou <rico@minet.net>
2019-04-27 00:02:37 +0200

    Rectification de l'affichage des ateliers

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 const session = require('express-session');
 //const passport = require('passport');
 //const LocalStrategy = require('passport-local').Strategy;
-const redis = require('redis');
-const redisStore = require('connect-redis')(session);
-const client = redis.createClient();
-//const router = express.Router();
+//const redis = require('redis');
+//const redisStore = require('connect-redis')(session);
+//const client = redis.createClient();
+
 
 
 // création du serveur
@@ -18,12 +18,14 @@ const app = express();
 
 app.use(session({
 	secret: 'debian >> ubuntu',
-	store: new redisStore({ host: 'localhost', port: 6379, client: client, ttl: 260}),
+	//store: new redisStore({ host: 'localhost', port: 6379, client: client, ttl: 260}),
 	saveUninitialized: false,
 	resave: false
 }));
 
 // configuration
+
+var http = require('http').Server(app)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 //app.use(cookieParser());
@@ -34,10 +36,12 @@ app.use(express.static('public'));
 app.set('view engine', 'jade');
 app.set('views', 'templates');
 
-//app.use('/', router);
+
+var router = express.Router();
 
 
 // Connection à la base de donnée
+
 var connection = mysql.createConnection({
 host : 'localhost',
 user : 'Citoyen',
@@ -48,25 +52,22 @@ database : 'passeportCitoyen'
 
 // routes
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('accueil');
 });
 
-app.get('/prof', (req, res) => {
+router.get('/prof', (req, res) => {
     res.render('prof');
 });
 
-app.post('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
   console.log('Inside POST / callback function')
   console.log(req.body)
   const name = req.body.username;
   const password = req.body.psw;
-  //req.session.name = req.body.username;
-  //req.session.password = req.body.psw;
-  //req.session.prof = req.body.profcb;
 })
 
-app.post('/passeport', (req, res) => {
+router.post('/passeport', (req, res) => {
     console.log('Inside POST /passeport callback')
     var name = req.body.username;
     var password = req.body.psw;
@@ -79,7 +80,7 @@ app.post('/passeport', (req, res) => {
       tmpl = 'prof';
     }
     var sql = 'SELECT * FROM ' + table + ' WHERE user = ' + connection.escape(name);
-	//console.log('sql vaut ' + sql);
+    //console.log('sql vaut ' + sql);
     connection.query(sql, function(err, results) {
     if (err) {
         //res.send("Error during MySql command : " + err);
@@ -90,13 +91,13 @@ app.post('/passeport', (req, res) => {
         var passwordDB = results[0].password;
         if (password == passwordDB) {
             console.log('Local strategy returned true')
-	    //req.session.name = name;
-	    //req.session.password = password;
- 	    //req.session.prof = req.body.profcb
-	    req.session.success = true;
-	    save_session(req, name, password, prof);	
+	    req.session.name = name;
+	    req.session.password = password;
+ 	    req.session.prof = req.body.profcb
+	    //req.session.success = true;
+	    //save_session(req, name, password, prof);	
 	    
-            res.render(tmpl, { username: name })
+            res.render(tmpl, { username: req.session.name })
         }
         else {
         //res.send("Wrong Password");
@@ -110,22 +111,17 @@ app.post('/passeport', (req, res) => {
       }
     }
     })
-	//req.session.name = name;
-	//req.session.password = password;
-	//req.session.prof = req.body.profcb;
-	var save_session = function(req, name, password, prof){
-		var test = 'test'
-		console.log('save session!!!!!!!!!!!!!' + name + password + prof + test)
-
-		req.session.name = name;
-		req.session.password = password;
+	//var save_session = function(req, name, password, prof){
+	//	req.session.name = name;
+	//	req.session.password = password;
 		//req.session.prof = prof    /!\ warning prof est undefined si false (unchecked)
-	}
+	//}
+	console.log(req.session);
 
 })
 
 
-app.get('/passeport', (req, res) => {
+router.get('/passeport', (req, res) => {
 	console.log('Inside GET /passeport callback')
 	if(req.session.name){
     		var table = 'loginEleve';
@@ -135,7 +131,7 @@ app.get('/passeport', (req, res) => {
       			table = 'loginProfesseur';
       			tmpl = 'prof';
     			}
-        res.render(tmpl, { username: name })
+        res.render(tmpl, { username: req.session.name })
         }
         else {
         res.redirect('/')
@@ -144,38 +140,33 @@ app.get('/passeport', (req, res) => {
 
 
 
-app.get('/passeport/:annee/:name', (req,res) => {
-  //TODO Faire la requête en base de données et mettre le résultat sous la forme [Nom atelier, description, validé = 1, non validé = 0]
-  //Pour cela récupérer l'année à partir de req.params.annee
-	var name = req.session.name
-	var id_eleve = 'SELECT id FROM loginEleve WHERE user = ' + connection.escape(req.params.name);
-	req.session.id = id_eleve;
-	var sqlAteliers = 'SELECT nomAtelier, description, réussite FROM listeAteliers JOIN ateliersSuivis ON listeAteliers.id=ateliersSuivis.atelier WHERE eleve = ' + connection.escape(id_eleve);
-	//res.send(connection.escape(sqlAteliers));
-    	connection.query(sqlAteliers, function(err, results) {
+router.get('/passeport/:annee/:name', (req,res) => {
+	
+	var sqlAteliers = 'SELECT nomAtelier, description, réussite FROM listeAteliers JOIN ateliersSuivis ON listeAteliers.id=ateliersSuivis.atelier WHERE eleve = (SELECT id FROM loginEleve WHERE user = ' + connection.escape(req.session.name) + ')';
+    	
+	connection.query(sqlAteliers, function(err, results) {
 		if (err){
-			res.send("Error during MySQL command: " + err);
+			//res.send("Error during MySQL command: " + err);
+			res.redirect('/passeport')
 		}
 		else {
 			var ateliers_list = new Array();
 			for (var i = 0; i < results.length; i++){
 				ateliers_list[i] = [ results[i].nomAtelier, results[i].description, results[i].réussite ];
 			}
-			res.send('le résultat est ' + ateliers_list); // TEST
+			//console.log('le résultat est ' + ateliers_list);
 			req.session.ateliers = ateliers_list;
-			//res.render('passeport', { annee: req.params.annee, username:req.params.name, ateliers: ateliers_list });
+			res.render('passeport', { annee: req.params.annee, username:req.params.name, ateliers: req.session.ateliers });
 		}
 	});
 		
-		//[ ['Atelier 1', 'Description atelier 1', 0], ['Atelier 2', 'Description atelier 2', 1], ['Atelier 3', 'Description atelier 3', 0] ] });
-
 });
 
 
 // Ecoute sur le port 8080
 
-//app.use('/', router);
+app.use('/', router);
 
-app.listen(8080, () => {
+http.listen(8080, () => {
     console.log('Listening on localhost:8080');
 })
